dd4d464913f4f4d620fadea58b6de7737bcb308d
Jamal Google <bassemgamal246@gmail.com>
2024-09-07 01:37:12 +0300

    Update Snake Gameplay.py

--- Snake Gameplay.py
+++ Snake Gameplay.py
@@ -1,5 +1,5 @@
 import pygame
-import sys
+import sys 
 import random
 
 # Initialize Pygame
@@ -13,7 +13,7 @@ BLOCK_SIZE = 20
 
 # Set up some colors
 BLACK = (0, 0, 0)
-GREEN = (57, 255, 20)
+GREEN = (57, 255, 20 )
 RED = (255, 0, 0)
 
 # Set up the display
@@ -24,6 +24,9 @@ pygame.display.set_caption('Snake Game')
 snake = [(200, 200), (220, 200), (240, 200)]
 food = (400, 300)
 direction = 'RIGHT'
+
+# Initialize the counter
+counter = 0
 
 # Game loop
 while True:
@@ -44,13 +47,13 @@ while True:
     # Move the snake
     head = snake[0]
     if direction == 'UP':
-        new_head = (head[0], (head[1] - BLOCK_SIZE) % HEIGHT)
+        new_head = (head[0], (head[1] - BLOCK_SIZE) % HEIGHT) #decrease the y coordinate
     elif direction == 'DOWN':
-        new_head = (head[0], (head[1] + BLOCK_SIZE) % HEIGHT)
+        new_head = (head[0], (head[1] + BLOCK_SIZE) % HEIGHT) #increase the y coordinate
     elif direction == 'LEFT':
-        new_head = ((head[0] - BLOCK_SIZE) % WIDTH, head[1])
+        new_head = ((head[0] - BLOCK_SIZE) % WIDTH, head[1]) #decrease the x coordinate
     elif direction == 'RIGHT':
-        new_head = ((head[0] + BLOCK_SIZE) % WIDTH, head[1])
+        new_head = ((head[0] + BLOCK_SIZE) % WIDTH, head[1]) #increase the x coordinate
 
     snake.insert(0, new_head)
 
@@ -58,6 +61,7 @@ while True:
     if snake[0] == food:
         food = (random.randrange(0, WIDTH - BLOCK_SIZE, BLOCK_SIZE), random.randrange(0, HEIGHT - BLOCK_SIZE, BLOCK_SIZE))
         SPEED += 1
+        counter += 1  # Increase the counter when food is eaten
     else:
         snake.pop()
 
@@ -66,6 +70,11 @@ while True:
     for pos in snake:
         pygame.draw.rect(screen, GREEN, pygame.Rect(pos[0], pos[1], BLOCK_SIZE, BLOCK_SIZE))
     pygame.draw.rect(screen, RED, pygame.Rect(food[0], food[1], BLOCK_SIZE, BLOCK_SIZE))
+
+    # Display the counter
+    font = pygame.font.Font(None, 36)
+    text = font.render(f'Score: {counter}', True, (255, 255, 255))
+    screen.blit(text, (10, 10))
 
     # Update the display
     pygame.display.flip()
